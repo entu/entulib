1b9879c77365ae3768c08744ce501d7777a6da73
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-01-18 10:37:50 +0200

    js promise

--- README.md
+++ README.md
@@ -5,77 +5,47 @@ entulib
 
 Library for authenticated access to Entu database API.
 
-Note.  
-If using entulib as git submodule, then update it with lines
-> git pull  
-> git submodule init  
-> git submodule update  
-> git submodule status  
-> git submodule foreach git pull origin master  
-> git submodule status  
-
-| Service Request | Description | HTTP Method | Py3 | NodeJS
+| Service Request | Description | HTTP Method
 |:---- |:---- |:---- |:---:|:---:|
-| Get Entity | Fetch Entity by ID | GET | + | + |
-| Get Childs | Description | GET | - | + |
-| Get Referrals | Description | GET | - | + |
-| Find Entity | Description | GET | + | + |
-| Create Entity | Description | POST | + | + |
-| Add Properties | Description | PUT | + | + |
-| Add File | Description | POST | + | + |
+| Get Entity | Fetch Entity by ID | GET |
+| Get Childs | Description | GET |
+| Get Referrals | Description | GET |
+| Find Entity | Description | GET |
+| Create Entity | Description | POST |
+| Add Properties | Description | PUT |
+| Add File | Description | POST |
 
 
 Authentication of application is done with
-    
+
   - API key
     - that is stored in Entu
       - and is attached to entity
         - that identifies the application
-    
+
 
 ## 7 javascript (5 python) methods to please your application
 
-### python 3
-#### class EntuLib():
-\__init__(self, entu_user_id, entu_user_key, entu_url):  
->
->  - def get_entity(self, entity_id):  
->    - on success return {result: entity, time: 0.009}
->    - else return {time: 0.009, error: "Entity with given ID is not found!"}
->  - def find_entity(self, definition, query, limit=1000000):
->    - on success return { count: count, result: [entitylist], time: 0.011}
->    - else return { count: 0, result: [], time: 0.086 }
->  - def create_entity(self, parent_id, definition, properties={}):
->    - on success return {result: { id: 685 }, time: 0.073}
->    - else return {time: 0.01, error: message}
->  - def add_properties(self, entity_id, definition, properties):
->    - on success return {result: { id: '686' }, time: 0.024}
->    - else return {time: 0.018, error: message}
->  - def add_file(self, entity_id, property_definition, abspath):
->    - on success return {time: 0.088}
->    - else return {time: 0.018, error: message}
 
-
-### javascript
 #### var EntuLib = function EntuLib(entu_user_id, entu_user_key, entu_url)
-> - getEntity: function (entity_id, callback)
->    - on success callback ({result: entity, time: 0.009})
->    - else callback ({time: 0.009, error: "Entity with given ID is not found!"})
-> - getChilds: function (entity_id, callback)
->    - on success callback ({count: count, result: {}, time: 0.097})
->    - else callback ({count: 0, result: {definition: {entities:[entities]}}, time: 0.094})
-> - getReferrals: function (entity_id, callback)
->    - on success callback ({result: entity, time: 0.009})
->    - else callback ({count: 0, result: {definition: {entities:[entities]}}, time: 0.095})
-> - findEntity: function (definition, query, limit, callback)
->    - on success callback ({ count: count, result: [entitylist], time: 0.011})
->    - else callback ({ count: 0, result: [], time: 0.086 })
-> - createEntity: function (parent_id, definition, properties, callback)
->    - on success callback ({result: { id: 685 }, time: 0.073})
->    - else callback ({time: 0.01, error: message})
-> - addProperties: function (entity_id, definition, properties, callback)
->    - on success callback ({result: { id: '686' }, time: 0.024})
->    - else callback ({time: 0.018, error: message})
-> - addFile: function (entity_id, property_definition, abspath, callback)
->    - on success callback ({time: 0.088})
->    - else callback ({time: 0.018, error: message})
+- getEntity: function (entity_id, callback)
+    - on success callback ({result: entity, time: 0.009})
+    - else callback ({time: 0.009, error: "Entity with given ID is not found!"})
+- getChilds: function (entity_id, callback)
+    - on success callback ({count: count, result: {}, time: 0.097})
+    - else callback ({count: 0, result: {definition: {entities:[entities]}}, time: 0.094})
+- getReferrals: function (entity_id, callback)
+    - on success callback ({result: entity, time: 0.009})
+    - else callback ({count: 0, result: {definition: {entities:[entities]}}, time: 0.095})
+- findEntity: function (definition, query, limit, callback)
+    - on success callback ({ count: count, result: [entitylist], time: 0.011})
+    - else callback ({ count: 0, result: [], time: 0.086 })
+- createEntity: function (parent_id, definition, properties, callback)
+    - on success callback ({result: { id: 685 }, time: 0.073})
+    - else callback ({time: 0.01, error: message})
+- addProperties: function (entity_id, definition, properties, callback)
+    - on success callback ({result: { id: '686' }, time: 0.024})
+    - else callback ({time: 0.018, error: message})
+- addFile: function (entity_id, property_definition, abspath, callback)
+    - on success callback ({time: 0.088})
+    - else callback ({time: 0.018, error: message})
---
+++ index.js
@@ -0,0 +1,270 @@
+var request   = require('request')
+var async     = require('async')
+var op        = require('object-path')
+var md        = require('marked')
+var crypto    = require('crypto')
+var Promise   = require('promise')
+// var Promise  = require('promise/lib/rejection-tracking').enable( {allRejections: true} )
+
+LIMIT_PARALLEL = 3
+
+function signData(data) {
+    data = data || {}
+
+    if (!APP_ENTU_USER || !APP_ENTU_KEY) { return data }
+
+    var conditions = []
+    for (k in data) {
+        if (data.hasOwnProperty(k)) {
+            conditions.push({k: data[k]})
+        }
+    }
+
+    var expiration = new Date()
+    expiration.setMinutes(expiration.getMinutes() + 10)
+
+    data.user = APP_ENTU_USER
+    var buffStr = JSON.stringify({expiration: expiration.toISOString(), conditions: conditions})
+    data.policy = new Buffer(buffStr).toString('base64')
+    data.signature = crypto.createHmac('sha1', APP_ENTU_KEY).update(data.policy).digest('base64')
+
+    return data
+}
+
+
+//Get entity from Entu
+function getEntity(id, authId, authToken) {
+    return new Promise(function (fulfill, reject) {
+        var headers = {}
+        var qs = {}
+        if (authId && authToken) {
+            headers = {'X-Auth-UserId': authId, 'X-Auth-Token': authToken}
+        } else {
+            qs = signData()
+        }
+
+        request.get({url: APP_ENTU_URL + '/entity-' + id, headers: headers, qs: qs, strictSSL: true, json: true}, function(err, response, body) {
+            if (err) {
+                return reject(err)
+            }
+            if (response.statusCode !== 200) {
+                return reject({error: op.get(body, 'error', body), eID: id, status: response.statusCode})
+            }
+            if (!body.result) {
+                return reject(op.get(body, 'error', body))
+            }
+            if (body.error) {
+                return reject(op.get(body, 'error', body))
+            }
+            var properties = op.get(body, 'result.properties', {})
+            var entity = {
+                id: op.get(body, 'result.id', null),
+                displayname: op.get(body, 'result.displayname', null),
+                displayinfo: op.get(body, 'result.displayinfo', null),
+                definition: op.get(body, 'result.definition.keyname', null),
+                picture: APP_ENTU_URL + '/entity-' + op.get(body, 'result.id', null) + '/picture',
+                right: op.get(body, 'result.right', null),
+                properties: {}
+            }
+            for (var p in properties) {
+                if (op.has(properties, [p, 'values'])) {
+                    for (var v in op.get(properties, [p, 'values'])) {
+                        if (op.get(properties, [p, 'datatype']) === 'file') {
+                            op.push(entity, ['properties', p], {
+                                id: op.get(properties, [p, 'values', v, 'id']),
+                                created: op.get(properties, [p, 'values', v, 'created']),
+                                'created_by': op.get(properties, [p, 'values', v, 'created_by']),
+                                value: op.get(properties, [p, 'values', v, 'value']),
+                                file: APP_ENTU_URL + '/file-' + op.get(properties, [p, 'values', v, 'db_value'])
+                            })
+                        } else if (op.get(properties, [p, 'datatype']) === 'text') {
+                            op.push(entity, ['properties', p], {
+                                id: op.get(properties, [p, 'values', v, 'id']),
+                                created: op.get(properties, [p, 'values', v, 'created']),
+                                'created_by': op.get(properties, [p, 'values', v, 'created_by']),
+                                value: op.get(properties, [p, 'values', v, 'value']),
+                                md: md(op.get(properties, [p, 'values', v, 'db_value']))
+                            })
+                        } else if (op.get(properties, [p, 'datatype']) === 'reference') {
+                            op.push(entity, ['properties', p], {
+                                id: op.get(properties, [p, 'values', v, 'id']),
+                                created: op.get(properties, [p, 'values', v, 'created']),
+                                'created_by': op.get(properties, [p, 'values', v, 'created_by']),
+                                value: op.get(properties, [p, 'values', v, 'value']),
+                                reference: op.get(properties, [p, 'values', v, 'db_value'])
+                            })
+                        } else {
+                            op.push(entity, ['properties', p], {
+                                id: op.get(properties, [p, 'values', v, 'id']),
+                                created: op.get(properties, [p, 'values', v, 'created']),
+                                'created_by': op.get(properties, [p, 'values', v, 'created_by']),
+                                value: op.get(properties, [p, 'values', v, 'value']),
+                            })
+                        }
+                    }
+                    if (op.get(properties, [p, 'multiplicity']) === 1) { op.set(entity, ['properties', p], op.get(entity, ['properties', p, 0])) }
+                }
+            }
+            fulfill(op(entity))
+        })
+    })
+}
+
+
+//Get entities by definition
+function getEntities(definition, limit, authId, authToken) {
+    return new Promise(function (fulfill, reject) {
+        if (!definition) { return reject(new Error('Missing "definition"')) }
+
+        var qs = {definition: definition}
+        var headers = {}
+        if (limit) { qs.limit = limit }
+        if (authId && authToken) {
+            headers = {'X-Auth-UserId': authId, 'X-Auth-Token': authToken}
+        } else {
+            qs = signData(qs)
+        }
+
+        request.get({url: APP_ENTU_URL + '/entity', headers: headers, qs: qs, strictSSL: true, json: true}, function(error, response, body) {
+            if (error) { return reject(error) }
+            if (response.statusCode !== 200 || !body.result) { return reject(new Error(op.get(body, 'error', body))) }
+
+            var entities = []
+            async.eachSeries(op.get(body, 'result', []), function(e, callback) {
+                getEntity(e.id, authId, authToken)
+                .then(function(opEntity) {
+                    entities.push(opEntity)
+                    callback()
+                })
+            }, function(error) {
+                if (error) { return reject(error) }
+                fulfill(entities)
+            })
+        })
+    })
+}
+
+
+//Get childs by parent entity id and optionally by definition
+function getChilds(parentEid, definition, authId, authToken) {
+    return new Promise(function (fulfill, reject) {
+        if (!parentEid) { return reject(new Error('Missing "parentEid"')) }
+        var qs = {}
+        if (definition) { qs = {definition: definition} }
+        var headers = {}
+        if (authId && authToken) {
+            headers = {'X-Auth-UserId': authId, 'X-Auth-Token': authToken}
+        } else {
+            qs = signData(qs)
+        }
+        var url = '/entity-' + parentEid + '/childs'
+        var options = {
+            url: APP_ENTU_URL + url,
+            headers: headers,
+            qs: qs,
+            strictSSL: true,
+            json: true
+        }
+        request.get(options, function(error, response, body) {
+            if (error) { return reject(error) }
+            if (response.statusCode !== 200 || !body.result) { return reject(new Error(op.get(body, 'error', body))) }
+            var definitions = Object.keys(body.result)
+            var childs = []
+            async.eachSeries(
+                definitions,
+                function doLoop(definition, doLoopCB) {
+                    var loop = ['result', definition, 'entities']
+                    async.each(op.get(body, loop, []), function(e, eachCB) {
+                        getEntity(e.id, authId, authToken)
+                        .then(function(childE) {
+                            childE.set('_display', {name: e.name, info: e.info})
+                            childs.push(childE)
+                            eachCB()
+                        })
+                    }, function gotByDef(error) {
+                        if (error) { return doLoopCB(error) }
+                        doLoopCB(null)
+                    })
+                },
+                function endLoop(error) {
+                    if (error) { return reject(error) }
+                    fulfill(childs)
+                }
+            )
+        })
+    })
+
+}
+
+
+//Edit entity
+// params = {
+//     entity_id: entity_id,
+//     entity_definition: entity_definition,
+//     dataproperty: dataproperty,
+//     property_id: property_id,
+//     new_value: new_value
+// }
+function edit(params) {
+    var body = {}
+    var property = params.entity_definition + '-' + params.dataproperty
+    if (op.get(params, ['property_id'], false)) {
+        property = property + '.' + params.property_id
+    }
+    body[property] = op.get(params, 'new_value', '')
+    var headers = {}
+    var qb = signData(body)
+    return new Promise(function (fulfill, reject) {
+        request.put(
+            { url: APP_ENTU_URL + '/entity-' + params.entity_id, headers: headers, body: qb, strictSSL: true, json: true, timeout: 60000 },
+            function(error, response, body) {
+                if(error) { return reject(error) }
+                if(response.statusCode !== 201 || !body.result) { return reject(new Error(op.get(body, 'error', body))) }
+                fulfill(op.get(body, 'result.properties.' + property + '.0', null))
+            }
+        )
+    })
+}
+
+//Add entity
+function add(parentEid, definition, properties, authId, authToken) {
+    var data = { definition: definition }
+
+    for (p in properties) {
+        if (properties.hasOwnProperty(p)) {
+            data[definition + '-' + p] = properties[p]
+        }
+    }
+
+    var headers = {}
+    var qb = data
+    if (authId && authToken) {
+        headers = {'X-Auth-UserId': authId, 'X-Auth-Token': authToken}
+    } else {
+        qb = signData(data)
+    }
+
+    var options = {
+        url: APP_ENTU_URL + '/entity-' + parentEid,
+        headers: headers,
+        body: qb,
+        strictSSL: true,
+        json: true
+    }
+    return new Promise(function (fulfill, reject) {
+        request.post(options, function(error, response, body) {
+            if (error) { return reject(error) }
+            if (response.statusCode !== 201 || !body.result) { return reject(new Error(op.get(body, 'error', body))) }
+            fulfill(op.get(body, 'result.id', null))
+        })
+    })
+}
+
+
+module.exports = {
+    getEntity: getEntity,
+    getChilds: getChilds,
+    getEntities: getEntities,
+    edit: edit,
+    add: add
+}
